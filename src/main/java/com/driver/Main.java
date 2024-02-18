35bdd0054de8816e030a901e92085b38e0c40103
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-02-18 14:10:39 +0530

    this is the project of polymorphisms

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-
+    static class Product{
+        public int product(int x , int y) {
+            return x*y;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new  Product();
+      int result =  p.product(2,4);
+      double result1 = p.product(5,10);
+        System.out.println(result);
+        System.out.println(result1);
+    }
 }
